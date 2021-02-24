5a61a6113e290b85d2c2e8a0a53f1923a48229d8
Samer-Alnajjar <samerk.alnajjar@gmail.com>
2021-02-24 17:16:43 +0200

    Finish the lab-08

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ const { search } = require("superagent");
 const app = express();
 app.use(cors());
 require("dotenv").config();
-const client = new pg.Client(process.env.DATABASE_URL);
-// const client = new pg.Client({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } });
+// const client = new pg.Client(process.env.DATABASE_URL);
+const client = new pg.Client({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } });
 
 const PORT = process.env.PORT;
 
